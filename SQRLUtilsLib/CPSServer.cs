d6be8a72f079fc2511d46f26132a4ffb66ef40cd
Jose C Gomez <jose@josecgomez.com>
2020-01-14 12:50:02 -0500

    Comments for CPS added

--- SQRLUtilsLib/CPSServer.cs
+++ SQRLUtilsLib/CPSServer.cs
@@ -12,16 +12,29 @@ using System.Web;
 
 namespace SQRLUtilsLib
 {
+    /// <summary>
+    /// This is a basic HTTP Server implemented to handle the CPS
+    /// requirements of SQRL
+    /// </summary>
     public class CPSServer
     {
         private Thread _serverThread;
         private HttpListener _listener;
+        
+        /// <summary>
+        /// Default SQRL CPS port do not change
+        /// </summary>
         private int Port { get; } =25519;
+
         public BlockingCollection<Uri> cpsBC;
         public string Nut { get; set; }
         public Uri Can { get; set; }
 
         public bool PendingResponse = false;
+
+        /// <summary>
+        /// Instanciates a CPS Http Server on the SQRL Default Port
+        /// </summary>
         public CPSServer()
         {
             this.cpsBC = new BlockingCollection<Uri>();
@@ -56,28 +69,36 @@ namespace SQRLUtilsLib
             }
         }
 
-
+        /// <summary>
+        /// Handles the Processing of any request received by the HTTP Server
+        /// </summary>
+        /// <param name="context"></param>
         private void Process(HttpListenerContext context)
         {
             Console.WriteLine("Processing Request");
             string filename = context.Request.Url.AbsolutePath;
             
             string extension = Path.GetExtension(filename);
+            //Checking if the request is for a gif. If it is we return a 1x1 clear gif and an OK response
             if(extension.Equals(".gif", StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine($"Got Gif request");
                 RespondWithGif(context);
                 Console.WriteLine($"Responded to Gif request");
             }
-            else
+            else //All other requests are handled as a CPS Redirect Request
             {
                 this.PendingResponse = true;
-                
                 RespondWithCPS(context);
                 this.PendingResponse = false;
             }
         }
 
+
+        /// <summary>
+        /// All Requests Processed by this Method are handled as a CPS Redirect Request
+        /// </summary>
+        /// <param name="context"></param>
         private void RespondWithCPS(HttpListenerContext context)
         {
             Console.WriteLine($"Got CPS Request");
@@ -85,16 +106,23 @@ namespace SQRLUtilsLib
             Sodium.SodiumCore.Init();
             Uri cpsData = new Uri(Encoding.UTF8.GetString(Sodium.Utilities.Base64ToBinary(data, "", Sodium.Utilities.Base64Variant.UrlSafeNoPadding)));
             var nvC= HttpUtility.ParseQueryString(cpsData.Query);
+
+            //Get the nut from the CPS Request
             if(nvC["nut"]!=null)
             {
                 this.Nut = nvC["nut"];
             }
+
+            //Try to get the Cancel url from the CPS Request
             if(nvC["can"]!=null)
             {
                 this.Can = new Uri(Encoding.UTF8.GetString(Sodium.Utilities.Base64ToBinary(nvC["can"],string.Empty,Sodium.Utilities.Base64Variant.UrlSafeNoPadding)));
             }
 
             Console.WriteLine($"Holding here till CPS is Ready");
+
+            //Hang here until we have a valid CPS then redirect the user
+            //Porbably need to figure out how to handle a timeout.
             foreach (var x in cpsBC.GetConsumingEnumerable())
             {
                 Console.WriteLine($"Redirecting To: {x}");
@@ -107,6 +135,10 @@ namespace SQRLUtilsLib
 
         }
 
+        /// <summary>
+        /// Returns a clear 1x1 gif to the user that requested it.
+        /// </summary>
+        /// <param name="context"></param>
         private void RespondWithGif(HttpListenerContext context)
         {
             try
@@ -131,8 +163,6 @@ namespace SQRLUtilsLib
 
         private byte[] GenerateGif()
         {
-            //do tracking stuff ....
-
             //return empty gif
             const string clearGif1X1 = "R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==";
             return Convert.FromBase64String(clearGif1X1);
