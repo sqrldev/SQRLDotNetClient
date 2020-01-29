0a01a1e5c8ed86027307ae5142813480cff71f8d
Jose C Gomez <jose@josecgomez.com>
2020-01-29 15:01:27 -0500

    Add Mac Installers

--- SQRLDotNetClientUI/ViewModels/ImportIdentitySetupViewModel.cs
+++ SQRLDotNetClientUI/ViewModels/ImportIdentitySetupViewModel.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SQRLDotNetClientUI.ViewModels
-{
+{ 
     public class ImportIdentitySetupViewModel : ViewModelBase
     {
         public SQRL sqrlInstance { get; set; }
