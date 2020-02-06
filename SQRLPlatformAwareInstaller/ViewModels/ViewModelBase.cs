76af740c2b6df6069ddd1fa55290492f67ab2cc3
Jose C Gomez <jose@josecgomez.com>
2020-02-06 15:47:36 -0500

    Initial Platform Insaller Project Creation

---
+++ SQRLPlatformAwareInstaller/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace SQRLPlatformAwareInstaller.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
