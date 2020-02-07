223a027f03db22c6a6bf8deb972da8fdd6b740d7
Alexander Hauser <alex@hausers.cc>
2020-02-07 20:57:57 +0100

    Add IdentitySettingsViewModel stub

---
+++ SQRLDotNetClientUI/ViewModels/IdentitySettingsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SQRLUtilsLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQRLDotNetClientUI.ViewModels
+{
+    class IdentitySettingsViewModel : ViewModelBase
+    {
+        public SQRL sqrlInstance { get; set; }
+        public SQRLIdentity Identity { get; set; }
+
+        public IdentitySettingsViewModel(SQRL sqrlInstance = null, SQRLIdentity identity = null)
+        {
+            this.Title = "SQRL Client - Identity Settings";
+            this.sqrlInstance = sqrlInstance;
+            this.Identity = identity;
+        }
+    }
+}
