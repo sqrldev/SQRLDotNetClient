7814635e3ccdf4d42b97ddb4c70e02f387afc386
Alexander Hauser <alex@hausers.cc>
2020-02-07 11:42:07 +0100

    Start documenting SQRLIdentity class

--- SQRLUtilsLib/SQRLIdentity.cs
+++ SQRLUtilsLib/SQRLIdentity.cs
@@ -5,35 +5,70 @@ using System.Linq;
 
 namespace SQRLUtilsLib
 {
+    /// <summary>
+    /// Represents a SQRL identity stored in the S4 format.
+    /// </summary>
+    /// <remarks>
+    /// More information about SQRL's binary storage format can be found 
+    /// at https://www.grc.com/sqrl/SQRL_Cryptography.pdf
+    /// </remarks>
     public class SQRLIdentity
     {
+        /// <summary>
+        /// The name of the identity.
+        /// </summary>
         public string IdentityName { get; set; }
 
+        /// <summary>
+        /// Creates a new <c>SQRLIdentity</c> object.
+        /// </summary>
+        /// <param name="name">The name of the identity (optional).</param>
         public SQRLIdentity(string name="") 
         {
             Blocks = new List<ISQRLBlock>();
             this.IdentityName = name;
         }
 
+        /// <summary>
+        /// The plaintext sqrl file header specified in the S4 storage format.
+        /// </summary>
         public const String SQRLHEADER = "sqrldata";
+        
+        /// <summary>
+        /// A list of blocks available in the identity.
+        /// </summary>
         public List<ISQRLBlock> Blocks { get; set; }
+
+        /// <summary>
+        /// The identity's type 1 block (User access password authenticated & encrypted data).
+        /// </summary>
         public SQRLBlock1 Block1 { get { return (SQRLBlock1)GetBlock(1); } }
+
+        /// <summary>
+        /// The identity's type 2 block (Rescue code encrypted data).
+        /// </summary>
         public SQRLBlock2 Block2 { get { return (SQRLBlock2)GetBlock(2); } }
+
+        /// <summary>
+        /// The identity's type 3 block (Encrypted previous identity unlock keys).
+        /// </summary>
         public SQRLBlock3 Block3 { get { return (SQRLBlock3)GetBlock(3); } }
 
         /// <summary>
-        /// Returns true if a block of the given type exists within the identity,
-        /// or false otherwise
+        /// Checks if a block of the given <paramref name="blockType"/> exists within the identity.
+        /// Returns <c>true</c> if it does, or <c>false</c> otherwise.
         /// </summary>
+        /// <param name="blockType">The block type of the block to be checked.</param>
         public bool HasBlock(ushort blockType)
         {
             return (GetBlock(blockType) != null);
         }
 
         /// <summary>
-        /// Returns the block with the given block type if it exists within the
-        /// identity, or null otherwise
+        /// Returns the block with the given <paramref name="blockType"/> if it 
+        /// exists within the identity, or <c>null</c> otherwise.
         /// </summary>
+        /// <param name="blockType">The block type of the block to be fetched.</param>
         public ISQRLBlock GetBlock(ushort blockType)
         {
             foreach (var block in Blocks) 
@@ -53,17 +88,20 @@ namespace SQRLUtilsLib
 
         /// <summary>
         /// Writes the raw byte representation of the current identity 
-        /// to a file with the given file name.
+        /// to a file with the given <paramref name="fileName"/>. If the
+        /// target file aleady exists, it is overwritten!
         /// </summary>
+        /// <param name="fileName">The full file path of the identity file to be created.</param>
         public void WriteToFile(string fileName)
         {
             File.WriteAllBytes(fileName, this.ToByteArray());
         }
 
         /// <summary>
-        /// Creates a SQRLIdentity object from the specified file 
+        /// Creates a SQRLIdentity object from the specified <paramref name="file"/>.
         /// </summary>
-        /// <returns>The imported identity object, or null if the file does not exist.</returns>
+        /// <param name="file">The full file path of the identity file to be parsed.</param>
+        /// <returns>The imported identity object, or <c>null</c> if the file does not exist.</returns>
         public static SQRLIdentity FromFile(string file)
         {
             if (!File.Exists(file)) return null;
@@ -73,15 +111,17 @@ namespace SQRLUtilsLib
         }
 
         /// <summary>
-        /// Parses a SQRL identity from the given raw byte array 
+        /// Parses a SQRL identity from the given <paramref name="identityData"/> byte array.
         /// </summary>
-        public static SQRLIdentity FromByteArray(byte[] identityData, bool texttual =false)
+        /// <param name="identityData">The raw byte representation of a SQRL identity.</param>
+        /// <param name="noHeader">Indicates whether the plaintext sqrl header is missing and checking for it should be skipped or not.</param>
+        public static SQRLIdentity FromByteArray(byte[] identityData, bool noHeader=false)
         {
             SQRLIdentity id = new SQRLIdentity();
             bool isBase64 = false;
             int skip = SQRLHEADER.Length;
             // Check header
-            if (!texttual)
+            if (!noHeader)
             {
                 string sqrlHeader = System.Text.Encoding.UTF8.GetString(identityData.Take(8).ToArray());
                 if (!sqrlHeader.Equals(SQRLHEADER, StringComparison.OrdinalIgnoreCase))
@@ -145,23 +185,44 @@ namespace SQRLUtilsLib
         }
     }
 
-
+    /// <summary>
+    /// Defines the miminumum requirements for a SQRL identity block 
+    /// adhering to the specification of the S4 identity storage format.
+    /// </summary>
     public interface ISQRLBlock
     {
-        byte[] ToByteArray();
+        /// <summary>
+        /// The whole block's length in bytes when stored in the S4 format.
+        /// </summary>
         ushort Length { get; }
+
+        /// <summary>
+        /// The block type as specified in the S4 identity storage format.
+        /// </summary>
         ushort Type { get; }
+
+        /// <summary>
+        /// Generates an identity block object by parsing the raw <paramref name="blockData"/> byte array.
+        /// </summary>
         void FromByteArray(byte[] blockData);
 
+        /// <summary>
+        /// Converts the identity block object into its raw byte representation.
+        /// </summary>
+        byte[] ToByteArray();
     }
 
     /// <summary>
-    /// Represents an "unknown" SQRL block type
+    /// Represents an "unknown" SQRL identity block type.
     /// </summary>
     public class SQRLBlock : ISQRLBlock
     {
         public ushort Length { get; set; }
         public ushort Type { get; set; }
+
+        /// <summary>
+        /// The raw data bytes following the block's length and type.
+        /// </summary>
         public byte[] BlockData { get; set; }
 
         public void FromByteArray(byte[] blockData)
@@ -190,8 +251,19 @@ namespace SQRLUtilsLib
     {
         public ushort Length { get; } = 125;
         public ushort Type { get; } = 1;
+
+        /// <summary>
+        ///  Length in bytes of the unencrypted (plaintext) but authenticated data for this
+        ///  block. This is the length of the so-called “additional authenticated data” (aad) 
+        ///  of the block's AESGCM cipher. The length specification spans from the first byte 
+        ///  of the block (the first byte of the whole block's length specification) up to, 
+        ///  but not including, the beginning of the block's encrypted data.
+        /// </summary>
         public ushort InnerBlockLength { get; } = 45;
 
+        /// <summary>
+        /// The AES-GCM initialization vector / nonce.
+        /// </summary>
         public byte[] ScryptInitVector { get; set; }
 
         public byte[] ScryptRandomSalt { get; set; }
