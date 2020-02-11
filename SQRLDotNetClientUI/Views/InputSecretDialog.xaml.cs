86c400222fe78452eaaca2813615ec40027b622a
Alexander Hauser <alex@hausers.cc>
2020-02-11 14:23:15 +0100

    Add keyboard focus control to InputSecretDialog

--- SQRLDotNetClientUI/Views/InputSecretDialog.xaml.cs
+++ SQRLDotNetClientUI/Views/InputSecretDialog.xaml.cs
@@ -1,22 +1,54 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace SQRLDotNetClientUI.Views
 {
     public class InputSecretDialog : Window
     {
+
+        private TextBox _txtSecret = null;
+        private Button _btnOK = null;
+
         public InputSecretDialog()
         {
             this.InitializeComponent();
+            _txtSecret = this.FindControl<TextBox>("txtSecret");
+            _btnOK = this.FindControl<Button>("btnOK");
+
+
+            _txtSecret.KeyUp += _txtSecret_KeyUp;
 #if DEBUG
             this.AttachDevTools();
 #endif
         }
 
+        private void _txtSecret_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Return) return;
+
+            RoutedEventArgs click = new RoutedEventArgs
+            {
+                RoutedEvent = Button.ClickEvent
+            };
+
+            this.RaiseEvent(click);
+            e.Handled = true;
+        }
+
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        protected override void OnOpened(EventArgs e)
+        {
+            base.OnOpened(e);
+            Application.Current.FocusManager.Focus(_txtSecret);
         }
     }
 }
