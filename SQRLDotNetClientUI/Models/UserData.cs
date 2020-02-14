87e97437316c1a7f5e850b8379a0c01abed68af5
Alexander Hauser <alex@hausers.cc>
2020-02-14 14:55:20 +0100

    Add documentation to UserData class

--- SQRLDotNetClientUI/Models/UserData.cs
+++ SQRLDotNetClientUI/Models/UserData.cs
@@ -4,9 +4,19 @@ using System.Text;
 
 namespace SQRLDotNetClientUI.Models
 {
+    /// <summary>
+    /// Stores user information such as the last loaded identity, etc.
+    /// </summary>
     public class UserData
     {
+        /// <summary>
+        /// The database record id.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// The file path of the last loaded identity.
+        /// </summary>
         public string LastLoadedIdentity { get; set; }
     }
 }
