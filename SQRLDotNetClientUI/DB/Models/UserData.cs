3fc074cfb2d236e5a824be588ed6bd99a57ca33f
Alexander Hauser <alex@hausers.cc>
2020-02-16 08:21:02 +0100

    Add db models

---
+++ SQRLDotNetClientUI/DB/Models/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQRLDotNetClientUI.DB.Models
+{
+    /// <summary>
+    /// Stores user information such as the last loaded identity, etc.
+    /// </summary>
+    public class UserData
+    {
+        /// <summary>
+        /// The database record id for the <c>UserData</c>.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The id of the last loaded identity.
+        /// </summary>
+        public string LastLoadedIdentity { get; set; }
+    }
+}
