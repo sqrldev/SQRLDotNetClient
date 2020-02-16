c37636a186d0ae8f432910fabf75e0f18a618354
Alexander Hauser <alex@hausers.cc>
2020-02-16 08:21:15 +0100

    Add DBContext

---
+++ SQRLDotNetClientUI/DB/DBContext/SQLiteDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQRLDotNetClientUI.DB.Models;
+using System;
+using System.IO;
+
+namespace SQRLDotNetClientUI.DB.DBContext
+{
+    public class SQLiteDBContext: DbContext
+    {
+        /// <summary>
+        /// Used for saving user state like last loaded identity etc. 
+        /// </summary>
+        public DbSet<UserData> UserData { get; set; }
+
+        /// <summary>
+        /// the list of imported identities.
+        /// </summary>
+        public DbSet<Identity> Identities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var directory = Path.GetDirectoryName(AppContext.BaseDirectory);
+            if (!File.Exists(Path.Combine(directory, "sqrl.db")))
+                directory = "";
+            Console.WriteLine($"{directory}: {File.Exists(Path.Combine(directory, "sqrl.db"))}");
+            options.UseSqlite($"Data Source={Path.Combine(directory,"sqrl.db")}");
+        }
+    }
+}
