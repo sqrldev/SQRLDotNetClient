7d87ce6760b8a765895c5bb443399b6f674f6617
Alexander Hauser <alex@hausers.cc>
2020-02-19 15:58:45 +0100

    Add new db migrations

---
+++ SQRLDotNetClientUI/Migrations/20200219133551_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SQRLDotNetClientUI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Identities",
+                columns: table => new
+                {
+                    UniqueId = table.Column<string>(nullable: false),
+                    GenesisId = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    DataBytes = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Identities", x => x.UniqueId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LastLoadedIdentity = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserData", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Identities");
+
+            migrationBuilder.DropTable(
+                name: "UserData");
+        }
+    }
+}
