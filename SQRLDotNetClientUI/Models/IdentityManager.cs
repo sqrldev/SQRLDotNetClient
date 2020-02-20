be4ea3478ecd3b0d03d6ca7ab793d22b1c539f99
Alexander Hauser <alex@hausers.cc>
2020-02-20 13:05:44 +0100

    Add IdentityManager.SetCurrentIdentity() plus some more improvements

--- SQRLDotNetClientUI/Models/IdentityManager.cs
+++ SQRLDotNetClientUI/Models/IdentityManager.cs
@@ -3,7 +3,10 @@ using SQRLDotNetClientUI.DB.Models;
 using SQRLUtilsLib;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace SQRLDotNetClientUI.Models
@@ -16,15 +19,17 @@ namespace SQRLDotNetClientUI.Models
     {
         private static readonly Lazy<IdentityManager> _instance = new Lazy<IdentityManager>(() => new IdentityManager());
         private SQRLDBContext _db;
+        private Identity _currentIdentity;
 
         /// <summary>
         /// The constructor is private because <c>IdentityManager</c> 
         /// implements the singleton pattern. To get an instance, use 
         /// <c>IdentityManager.Instance</c> instead.
         /// </summary>
-        private IdentityManager() 
+        private IdentityManager()
         {
             _db = new SQRLDBContext();
+            _currentIdentity = GetIdentityInternal(GetUserData().LastLoadedIdentity);
         }
 
         /// <summary>
@@ -46,9 +51,28 @@ namespace SQRLDotNetClientUI.Models
         {
             get
             {
-                return GetIdentity(
-                    GetUserData().LastLoadedIdentity);
+                if (_currentIdentity == null) return null;
+                return DeserializeIdentity(_currentIdentity.DataBytes);
             }
+        }
+
+        /// <summary>
+        /// Tries to set the identity with the given <paramref name="uniqueId"/> as
+        /// the currently active identity.
+        /// </summary>
+        /// <param name="uniqueId">The unique id of the identity to be set active.</param>
+        public void ChangeCurrentIdentity(string uniqueId)
+        {
+            if (_currentIdentity?.UniqueId == uniqueId) return;
+
+            Identity id = GetIdentityInternal(uniqueId);
+            if (id == null) throw new ArgumentException("No matching identity found!", nameof(uniqueId));
+
+            _currentIdentity = id;
+            GetUserData().LastLoadedIdentity = id.UniqueId;
+            _db.SaveChanges();
+
+            IdentityChanged?.Invoke(this, new IdentityChangedEventArgs(id.Name, id.UniqueId));
         }
 
         /// <summary>
@@ -63,38 +87,71 @@ namespace SQRLDotNetClientUI.Models
             newIdRec.Name = identity.IdentityName;
             newIdRec.UniqueId = identity.Block0.UniqueIdentifier.ToHex();
             newIdRec.GenesisId = identity.Block0.GenesisIdentifier.ToHex();
-            newIdRec.DataBytes = identity.ToByteArray();
+
+            // Serialize the identity for storing it in the database.
+            // We could use identity.ToByteArray() here, but then we would
+            // lose extra information not covered by the S4 format, such
+            // as identity name, file path etc.
+            newIdRec.DataBytes = SerializeIdentity(identity);
 
             _db.Identities.Add(newIdRec);
             _db.SaveChanges();
 
             if (setAsCurrentIdentity)
             {
-                UserData ud = GetUserData();
-                ud.LastLoadedIdentity = newIdRec.UniqueId;
-                _db.SaveChanges();
+                ChangeCurrentIdentity(newIdRec.UniqueId);
             }
         }
 
         /// <summary>
         /// Retrieves the <c>SQRLIdentity</c> with the given <paramref name="uniqueId"/>
-        /// from the database.
+        /// from the database. If no such identity was found, the method returns <c>null</c>.
         /// </summary>
-        /// <param name="uniqueId">The unique id found in block type 0 of the identity to be returned.</param>
+        /// <param name="uniqueId">The unique id of the identity to be returned.</param>
         public SQRLIdentity GetIdentity(string uniqueId)
+        {
+            Identity id = GetIdentityInternal(uniqueId);
+            if (id == null) return null;
+
+            return DeserializeIdentity(id.DataBytes);
+        }
+
+        /// <summary>
+        /// Returns a list of available identities (name and unique id).
+        /// </summary>
+        /// <returns>A list of <c>Tuple</c>s, each containing two strings, where the first 
+        /// one is the identity's name and the second is the identity's unique id.</returns>
+        public List<Tuple<string, string>> GetIdentities()
+        {
+            List<Tuple<string, string>> result = new List<Tuple<string, string>>();
+
+            foreach (var id in _db.Identities)
+                result.Add(new Tuple<string, string>(id.Name, id.UniqueId));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Retrieves the <c>Identity</c> with the given <paramref name="uniqueId"/>
+        /// from the database. If no such identity was found, the method returns <c>null</c>.
+        /// </summary>
+        /// <param name="uniqueId">The unique id of the identity to be returned.</param>
+        private Identity GetIdentityInternal(string uniqueId)
         {
             if (_db.Identities.Count() < 1) return null;
 
-            var id = _db.Identities
+            return _db.Identities
                 .Single(i => i.UniqueId == uniqueId);
-
-            return SQRLIdentity.FromByteArray(id.DataBytes, false);
         }
 
+        /// <summary>
+        /// Returns the <c>UserData</c> database entry. If no such entry
+        /// exists, it will be created.
+        /// </summary>
         private UserData GetUserData()
         {
             UserData result = null;
-            
+
             result = _db.UserData.FirstOrDefault();
             if (result == null)
             {
@@ -106,6 +163,49 @@ namespace SQRLDotNetClientUI.Models
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Serializes a <c>SQRLIdentity</c> object to a byte array.
+        /// </summary>
+        /// <param name="identity">The identity to be serialized.</param>
+        private byte[] SerializeIdentity(SQRLIdentity identity)
+        {
+            byte[] identityBtes;
+            IFormatter formatter = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, identity);
+                identityBtes = stream.ToArray();
+            }
+            return identityBtes;
+        }
+
+        /// <summary>
+        /// Deserializes a <c>SQRLIdentity</c> object from a byte array.
+        /// </summary>
+        /// <param name="dataBytes">The byte array representing the serialized <c>SQRLIdentity</c>.</param>
+        private SQRLIdentity DeserializeIdentity(byte[] dataBytes)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            return (SQRLIdentity)formatter.Deserialize(new MemoryStream(dataBytes));
+        }
+
+        /// <summary>
+        /// This event is raised if the currently selected identity changes.
+        /// </summary>
+        public event EventHandler<IdentityChangedEventArgs> IdentityChanged;
+    }
+
+    public class IdentityChangedEventArgs : EventArgs
+    {
+        public string IdentityName { get; }
+        public string IdentityUniqueId { get; }
+
+        public IdentityChangedEventArgs(string identityName, string identityUniqueId)
+        {
+            IdentityName = identityName;
+            IdentityUniqueId = identityUniqueId;
         }
     }
 }
