b9ed8d03078eaccc0afce817ec26d8561c1417d2
Alexander Hauser <alex@hausers.cc>
2020-02-28 20:04:02 +0100

    Add IdentityManager instance field to ViewModelBase

--- SQRLDotNetClientUI/ViewModels/ViewModelBase.cs
+++ SQRLDotNetClientUI/ViewModels/ViewModelBase.cs
@@ -1,10 +1,12 @@
 using Avalonia;
 using ReactiveUI;
 using SQRLDotNetClientUI.AvaloniaExtensions;
+using SQRLDotNetClientUI.Models;
 using SQRLDotNetClientUI.Views;
 
 public class ViewModelBase : ReactiveObject
 {
+    protected IdentityManager _identityManager = IdentityManager.Instance;
     protected MainWindow _mainWindow = AvaloniaLocator.Current.GetService<MainWindow>();
     protected LocalizationExtension _loc = AvaloniaLocator.Current.GetService<MainWindow>().LocalizationService;
 
