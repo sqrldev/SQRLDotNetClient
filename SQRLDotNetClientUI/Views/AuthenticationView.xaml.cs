52671f4b820dfe59225e36c703d9e0d52358d79a
Alexander Hauser <alex@hausers.cc>
2020-03-01 21:06:55 +0100

    Optimize focus behaviour in AuthenticationView

--- SQRLDotNetClientUI/Views/AuthenticationView.xaml.cs
+++ SQRLDotNetClientUI/Views/AuthenticationView.xaml.cs
@@ -1,21 +1,45 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using SQRLDotNetClientUI.AvaloniaExtensions;
 using SQRLDotNetClientUI.ViewModels;
 
 namespace SQRLDotNetClientUI.Views
 {
     public class AuthenticationView : UserControl
     {
+        private CopyPasteTextBox _txtPassword = null;
+        private CopyPasteTextBox _txtAltID = null;
+
         public AuthenticationView()
         {
             this.InitializeComponent();
-            
+            _txtPassword = this.FindControl<CopyPasteTextBox>("txtPassword");
+            _txtAltID = this.FindControl<CopyPasteTextBox>("txtAltID");
+            this.GotFocus += AuthenticationView_GotFocus;
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void AuthenticationView_GotFocus(object sender, Avalonia.Input.GotFocusEventArgs e)
+        {
+            if (e?.Source is Button)
+            {
+                if (((Button)e.Source).Name == "btnIdentitySelector")
+                {
+                    _txtPassword.Focus();
+                    e.Handled = true;
+                }
+
+                if (((Button)e.Source).Name == "btnAdvancedFunctions")
+                {
+                    _txtAltID.Focus();
+                    e.Handled = true;
+                }
+            }
         }
     }
 }
