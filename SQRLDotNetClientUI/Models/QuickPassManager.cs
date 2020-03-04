8af162227e6a9727a3923e0d34c85478b95d42a3
Alexander Hauser <alex@hausers.cc>
2020-03-04 11:43:29 +0100

    Improve QuickPassManager and add some more logging

--- SQRLDotNetClientUI/Models/QuickPassManager.cs
+++ SQRLDotNetClientUI/Models/QuickPassManager.cs
@@ -16,8 +16,12 @@ namespace SQRLDotNetClientUI.Models
         private static readonly Lazy<QuickPassManager> _instance = new Lazy<QuickPassManager>(() => new QuickPassManager());
         private IdentityManager _identityManager = IdentityManager.Instance;
         private object _dataSyncObj = new object();
-        private Dictionary<string, QuickPassInfo> _data = new Dictionary<string, QuickPassInfo>();
-        private ILogger _log = Log.ForContext(typeof(QuickPassManager));
+        private Dictionary<string, QuickPassItem> _quickPassItems = new Dictionary<string, QuickPassItem>();
+
+        /// <summary>
+        /// The number of seconds the QuickPass shall be run through the PBKDF.
+        /// </summary>
+        private const int QP_KEYDERIV_SEC = 1;
 
         /// <summary>
         /// Returns the singleton <c>QuickPassManager</c> instance. If 
@@ -44,11 +48,13 @@ namespace SQRLDotNetClientUI.Models
             // Register the identity changed event handler
             _identityManager.IdentityChanged += _identityManager_IdentityChanged;
 
-            _log.Information("QuickPassManager initialized.");
+            Log.Information("QuickPassManager initialized.");
         }
 
         ~QuickPassManager()
         {
+            Log.Information("QuickPassManager desctructor called");
+
             // Unregister the identity changed event handler
             _identityManager.IdentityChanged -= _identityManager_IdentityChanged;
 
@@ -57,26 +63,39 @@ namespace SQRLDotNetClientUI.Models
         }
 
         /// <summary>
-        /// This event handler gets called if 
+        /// This event handler gets called when the currently selected identity changes.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void _identityManager_IdentityChanged(object sender, IdentityChangedEventArgs e)
         {
             if (ClearQuickPassOnIdentityChange)
             {
+                Log.Information("Clearing all QuickPass entries because of identity change");
                 ClearAllQuickPass();
             }
         }
 
         /// <summary>
-        /// Returns <c>true</c> if a QuickPass for the currently active identity
-        /// is currently stored in memory, or <c>false</c> otherwise.
+        /// Returns <c>true</c> if a QuickPass for the given <paramref name="identityUniqueId"/>
+        /// is currently stored in memory, or <c>false</c> otherwise. To check for the currenty
+        /// active identity, just pass <c>null</c> for <paramref name="identityUniqueId"/>.
         /// </summary>
-        public bool HasQuickPass()
+        /// <param name="identityUniqueId">The hex representation of the identity's unique id (block 0)
+        /// for which to check the QuickPass status, or <c>null</c> if you want to check for the current identity.</param>
+        public bool HasQuickPass(string identityUniqueId = null)
         {
-            // TODO: Implement
-            return true;
+            if (identityUniqueId == null)
+                identityUniqueId = _identityManager.CurrentIdentity?.Block0?.UniqueIdentifier?.ToHex();
+
+            if (identityUniqueId == null)
+                return false;
+
+            bool hasQuickPass;
+            lock (_dataSyncObj)
+            {
+                hasQuickPass = _quickPassItems.ContainsKey(identityUniqueId);
+            }
+
+            return hasQuickPass;
         }
 
         /// <summary>
@@ -91,15 +110,15 @@ namespace SQRLDotNetClientUI.Models
         /// <param name="identity">The identity that the QuickPass should be set for.</param>
         /// <param name="progress">An object implementing the IProgress interface for tracking the operation's progress (optional).</param>
         /// <param name="progressText">A string representing a text descrition for the progress indicator (optional).</param>
-        public async void SetQuickPass(string password, byte[] imk, SQRLIdentity identity, IProgress<KeyValuePair<int, string>> progress = null, string progressText = null)
+        public async Task<bool> SetQuickPass(string password, byte[] imk, SQRLIdentity identity, IProgress<KeyValuePair<int, string>> progress = null, string progressText = null)
         {
-            QuickPassInfo qpi = new QuickPassInfo()
+            QuickPassItem qpi = new QuickPassItem()
             {
                 EstablishedDate = DateTime.Now,
                 QuickPassLength = identity.Block1.HintLength,
                 IdentityUniqueId = identity.Block0.UniqueIdentifier.ToHex(),
                 ScryptRandomSalt = SodiumCore.GetRandomBytes(16),
-                InitVector = SodiumCore.GetRandomBytes(16),
+                Nonce = SodiumCore.GetRandomBytes(24),
                 QuickPassTimeoutSecs = identity.Block1.PwdTimeoutMins * 60,
                 Timer = new Timer()
             };
@@ -110,16 +129,33 @@ namespace SQRLDotNetClientUI.Models
             qpi.Timer.Elapsed += _timer_Elapsed;
 
             string quickPass = password.Substring(0, qpi.QuickPassLength);
-            KeyValuePair<int, byte[]> enscryptResult = await SQRL.EnScryptTime(quickPass, qpi.ScryptRandomSalt, (int)Math.Pow(2, 9), 1, progress, progressText);
+
+            KeyValuePair<int, byte[]> enscryptResult = await SQRL.EnScryptTime(
+                quickPass, 
+                qpi.ScryptRandomSalt, 
+                (int)Math.Pow(2, 9), 
+                QP_KEYDERIV_SEC, 
+                progress, 
+                progressText);
 
             qpi.ScryptIterationCount = enscryptResult.Key;
-            qpi.EncryptedImk = StreamEncryption.Encrypt(imk, qpi.InitVector, enscryptResult.Value);
+            qpi.EncryptedImk = StreamEncryption.Encrypt(imk, qpi.Nonce, enscryptResult.Value);
 
+            // If we already have a QuickPass entry for this identity, remove it first
+            if (HasQuickPass(qpi.IdentityUniqueId)) 
+                ClearQuickPass(qpi.IdentityUniqueId);
+
+            // Now, add the QuickPass item to our list and start the timer
             lock (_dataSyncObj)
             {
-                _data.Add(qpi.IdentityUniqueId, qpi);
+                _quickPassItems.Add(qpi.IdentityUniqueId, qpi);
                 qpi.Timer.Start();
             }
+
+            Log.Information("QuickPass set for identity {IdentityUniqueId}", 
+                qpi.IdentityUniqueId);
+
+            return true;
         }
 
         /// <summary>
@@ -133,26 +169,44 @@ namespace SQRLDotNetClientUI.Models
         /// or <c>null</c> if you want to decrypt the current identity's IMK.</param>
         /// <param name="progress">An object implementing the IProgress interface for tracking the operation's progress (optional).</param>
         /// <param name="progressText">A string representing a text descrition for the progress indicator (optional).</param>
-        public async Task<byte[]> GetQuickPassDecryptedImk(string quickPass, string identityUniqueId = null, IProgress<KeyValuePair<int, string>> progress = null, string progressText = null)
+        /// <returns>The decrypted Identity Master Key for the given <paramref name="identityUniqueId"/>, or
+        /// <c>null</c> if no such QuickPass entry exists.</returns>
+        public async Task<byte[]> GetQuickPassDecryptedImk(string quickPass, string identityUniqueId = null, 
+            IProgress<KeyValuePair<int, string>> progress = null, string progressText = null)
         {
-            QuickPassInfo qpi = null;
+            QuickPassItem qpi = null;
 
             if (identityUniqueId == null)
                 identityUniqueId = _identityManager.CurrentIdentity?.Block0?.UniqueIdentifier?.ToHex();
 
             if (identityUniqueId == null)
+            {
+                Log.Error("Could not resolve current identity in {MethodName}, throwing Exception!", 
+                    nameof(GetQuickPassDecryptedImk));
+
                 throw new InvalidOperationException("Cannot return QuickPass-decrypted IMK without an identity!");
+            }
             
             lock (_dataSyncObj)
             {
-                if (!_data.ContainsKey(identityUniqueId))
-                    throw new ArgumentException("No QuickPass entry was found for the given identity unique id!");
+                if (!_quickPassItems.ContainsKey(identityUniqueId))
+                {
+                    Log.Warning("No identity found for id {IdentityUniqueId} in {MethodName}", 
+                        identityUniqueId, nameof(GetQuickPassDecryptedImk));
 
-                qpi = _data[identityUniqueId];
+                    return null;
+                }
+
+                qpi = _quickPassItems[identityUniqueId];
             }
 
-            byte[] key = await SQRL.EnScryptCT(quickPass, qpi.ScryptRandomSalt, (int)Math.Pow(2, 9), qpi.ScryptIterationCount, progress, progressText);
-            byte[] decryptedImk = StreamEncryption.Decrypt(qpi.EncryptedImk, qpi.InitVector, key);
+            byte[] key = await SQRL.EnScryptCT(quickPass, qpi.ScryptRandomSalt, (int)Math.Pow(2, 9), 
+                qpi.ScryptIterationCount, progress, progressText);
+
+            byte[] decryptedImk = StreamEncryption.Decrypt(qpi.EncryptedImk, qpi.Nonce, key);
+
+            Log.Information("QuickPass retrieved for identity {IdentityUniqueId}",
+                identityUniqueId);
 
             return decryptedImk;
         }
@@ -167,12 +221,14 @@ namespace SQRLDotNetClientUI.Models
         /// QuickPass entries affected, but instead raise a combined event.</param>
         public void ClearAllQuickPass(bool combineEvents = true)
         {
+            Log.Verbose("{MethodName} called", nameof(ClearAllQuickPass));
+
             List<string> uniqueIdsAvailable = null;
             List<string> uniqueIdsCleared = new List<string>();
 
             lock (_dataSyncObj)
             {
-                uniqueIdsAvailable = new List<string>(_data.Keys);
+                uniqueIdsAvailable = new List<string>(_quickPassItems.Keys);
             }
 
             foreach (string uniqueId in uniqueIdsAvailable)
@@ -188,6 +244,7 @@ namespace SQRLDotNetClientUI.Models
             if (combineEvents && uniqueIdsCleared.Count > 0)
             {
                 QuickPassCleared?.Invoke(this, new QuickPassClearedEventArgs(uniqueIdsCleared));
+                Log.Information("Fired combined ClickPassCleared event");
             }
         }
 
@@ -203,10 +260,17 @@ namespace SQRLDotNetClientUI.Models
         /// <paramref name="identityUniqueId"/> could be successfully cleared.</param>
         public bool ClearQuickPass(string identityUniqueId, bool fireClearedEvent = true)
         {
+            Log.Verbose("{MethodName} called", nameof(ClearQuickPass));
+
             lock (_dataSyncObj)
             {
-                if (!_data.ContainsKey(identityUniqueId)) return false;
-                var qpi = _data[identityUniqueId];
+                if (!_quickPassItems.ContainsKey(identityUniqueId))
+                {
+                    Log.Warning("No QuickPass entry found for identity {IdentityUniqueId}", identityUniqueId);
+                    return false;
+                }
+
+                QuickPassItem qpi = _quickPassItems[identityUniqueId];
 
                 // First, stop the QuickPass timer
                 qpi.Timer.Stop();
@@ -215,12 +279,19 @@ namespace SQRLDotNetClientUI.Models
                 // traces of key material in RAM.
                 qpi.EncryptedImk.ZeroFill();
 
-                // Finally, delete the QuickPass entry from our dictionary
-                _data.Remove(identityUniqueId);
+                // Delete the QuickPass entry from our dictionary
+                _quickPassItems.Remove(identityUniqueId);
             }
 
+            Log.Information("QuickPass entry cleared for identity id {IdentityUniqueId}",
+                identityUniqueId);
+
             // Finally, fire the QuickPassCleared event
-            if (fireClearedEvent) QuickPassCleared?.Invoke(this, new QuickPassClearedEventArgs(new List<string>() { identityUniqueId }));
+            if (fireClearedEvent)
+            {
+                QuickPassCleared?.Invoke(this, new QuickPassClearedEventArgs(new List<string>() { identityUniqueId }));
+                Log.Information("Firing QuickPassCleared event");
+            }
 
             return true;
         }
@@ -231,13 +302,13 @@ namespace SQRLDotNetClientUI.Models
         /// </summary>
         private void _timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            QuickPassInfo qpi = null;
+            QuickPassItem qpi = null;
             Timer timer = (Timer)sender;
 
             // Get the QuickPassInfo that this Timer belongs to
             lock (_dataSyncObj)
             {
-                foreach (var item in _data)
+                foreach (var item in _quickPassItems)
                 {
                     if (timer == item.Value.Timer)
                     {
@@ -246,6 +317,9 @@ namespace SQRLDotNetClientUI.Models
                     }
                 }
             }
+
+            Log.Information("QuckPass timer elapsed for identity id {IdentityUniqueId}",
+                qpi?.IdentityUniqueId);
 
             // If such a QuickPassInfo item exists, clear it
             if (qpi != null)
@@ -263,7 +337,7 @@ namespace SQRLDotNetClientUI.Models
     /// <summary>
     /// This class acts as a container for QuickPass management information.
     /// </summary>
-    public class QuickPassInfo
+    public class QuickPassItem
     {
         public byte[] EncryptedImk;
         public DateTime EstablishedDate;
@@ -271,7 +345,7 @@ namespace SQRLDotNetClientUI.Models
         public int QuickPassTimeoutSecs;
         public int ScryptIterationCount;
         public byte[] ScryptRandomSalt;
-        public byte[] InitVector;
+        public byte[] Nonce;
         public Timer Timer;
         public string IdentityUniqueId;
     }
