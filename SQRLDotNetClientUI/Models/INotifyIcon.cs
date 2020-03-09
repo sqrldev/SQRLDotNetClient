d15ae32aec60e46da38b7cd895c6a0245c297b36
Alexander Hauser <alex@hausers.cc>
2020-03-09 06:50:26 +0100

    Add INotifyIcon interface

---
+++ SQRLDotNetClientUI/Models/INotifyIcon.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQRLDotNetClientUI.Models
+{
+    public interface INotifyIcon
+    {
+        public void Show();
+        public void Hide();
+        public void Remove();
+        
+        public string IconPath { get; set; }
+        public string ToolTipText { get; set; }
+
+        public bool Visible { get; set; }
+
+        public event EventHandler<EventArgs> Click;
+        public event EventHandler<EventArgs> DoubleClick;
+    }
+}
