cb0994939176cbdc96774210629c78f4eafcb4e8
Alexander Hauser <alex@hausers.cc>
2020-03-11 06:42:08 +0100

    Register SystemEventNotifier in Implementations class

--- SQRLDotNetClientUI/Platform/Implementation.cs
+++ SQRLDotNetClientUI/Platform/Implementation.cs
@@ -7,17 +7,18 @@ using System.Text;
 namespace SQRLDotNetClientUI.Platform
 {
     /// <summary>
-    /// Provides access to platform-specific implementations.
+    /// Provides a central place for accessing platform-specific implementations.
+    /// New platform-specific implementations should be registered here.
     /// </summary>
     public static class Implementation
     {
         /// <summary>
         /// Returns a <c>Type</c> that represents the platform-specific implementation for 
-        /// the given type <typeparamref name="T"/>.
+        /// the given type <typeparamref name="T"/>, or <c>null</c> if no implementation
+        /// exists for the current platform.
         /// </summary>
         /// <typeparam name="T">The <c>Type</c> (mostly an interface type) to get a platform-
         /// specific implementation for.</typeparam>
-        /// <returns></returns>
         public static Type ForType<T>()
         {
             if (typeof(T) == typeof(INotifyIcon))
@@ -30,8 +31,21 @@ namespace SQRLDotNetClientUI.Platform
                     return null;
                 else return null;
             }
-            
-            throw new NotImplementedException("This type does not have a platform-specific implementation!");
+
+            if (typeof(T) == typeof(ISystemEventNotifier))
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    return typeof(Win.SystemEventNotifier);
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    return null;
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    return null;
+                else return null;
+            }
+
+            throw new NotImplementedException(
+                String.Format("No platform-specific implementations registered for type {0}!",
+                typeof(T).ToString()));
         }
     }
 }
