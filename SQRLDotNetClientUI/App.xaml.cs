c7a0df737b7ead54950becaeca48c222b8f5d8e2
Jose C Gomez <jose@josecgomez.com>
2020-03-12 15:07:28 -0400

    ETO test

--- SQRLDotNetClientUI/App.xaml.cs
+++ SQRLDotNetClientUI/App.xaml.cs
@@ -35,6 +35,11 @@ namespace SQRLDotNetClientUI
                     NSApplication.Init();
                     NSApplication.SharedApplication.Delegate = new Utils.AppDelegate((MainWindow)desktop.MainWindow);
                 }
+                else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    new Eto.Forms.Application(Eto.Platforms.Gtk).Attach();
+                  
+                }
             }          
 
             base.OnFrameworkInitializationCompleted();
