202403c5a8e11d0e4920e4d3280c0b5f4e81737a
Jose C Gomez <jose@josecgomez.com>
2020-03-13 09:23:29 -0400

    Added Notification Icon Implementation for Linux

--- SQRLDotNetClientUI/Platform/Linux/LinuxTrayIcon.cs
+++ SQRLDotNetClientUI/Platform/Linux/LinuxTrayIcon.cs
@@ -8,29 +8,19 @@ namespace SQRLDotNetClientUI.Platform.Linux
 {
     public class LinuxTrayIcon: Eto.Forms.Form
     {
-        private TrayIndicator _tray;
+        public TrayIndicator _tray;
         private bool _startup = true;
         public LinuxTrayIcon()
         {
-            Title = "My Eto Form";
+            
             ClientSize = new Size(200, 200);
             _tray = new TrayIndicator
             {
                 Image = Eto.Drawing.Icon.FromResource("SQRLDotNetClientUI.Assets.sqrl_icon_normal_256_32_icon.ico"),
-                Menu = new ContextMenu
-                {
-                    Items =
-               {
-                  new ButtonMenuItem {Text = "About...", Command = new Command((s, e) =>
-                  {
-                     Console.WriteLine("Things!");
-                  })},
-                  new ButtonMenuItem {Text = "Quit", Command = new Command((s, e) => Application.Instance.Quit())}
-               }
-                }
+                Menu = new ContextMenu()
             };
 
-            ShowInTaskbar = false;
+            _tray.Show();
             _tray.Visible = true;
         }
 
@@ -40,6 +30,12 @@ namespace SQRLDotNetClientUI.Platform.Linux
             {
                 Visible=false;
             }
+        }
+
+        protected override void OnUnLoad(EventArgs e)
+        {
+            base.OnUnLoad(e);
+            _tray.Hide();
         }
 
     }
