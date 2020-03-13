84216efe2d4949cdff523935ea071cc0b272b99d
Alexander Hauser <alex@hausers.cc>
2020-03-13 16:04:00 +0100

    Update ConsoleTester project to use the new tuple substitues

--- SQRLConsoleTester/Program.cs
+++ SQRLConsoleTester/Program.cs
@@ -33,8 +33,8 @@ namespace SQRLConsoleTester
             bool run =true;
             
            
-            var decryptedData = await SQRL.DecryptBlock1(newId, "Zingo-Bingo-Slingo-Dingo", progress);
-            if (decryptedData.Item1)
+            var block1Keys = await SQRL.DecryptBlock1(newId, "Zingo-Bingo-Slingo-Dingo", progress);
+            if (block1Keys.DecryptionSucceeded)
             {
                 try
                 {
@@ -46,11 +46,11 @@ namespace SQRLConsoleTester
                         string AltID = "";
 
 
-                        var siteKvp = SQRL.CreateSiteKey(requestURI, AltID, decryptedData.Item2);
+                        var siteKvp = SQRL.CreateSiteKey(requestURI, AltID, block1Keys.Imk);
                         Dictionary<byte[],Tuple<byte[],KeyPair>> priorKvps = null;
                         if(newId.Block3!=null && newId.Block3.Edition>0)
                         {
-                            byte[] decryptedBlock3 = SQRL.DecryptBlock3(decryptedData.Item2, newId, out bool allGood);
+                            byte[] decryptedBlock3 = SQRL.DecryptBlock3(block1Keys.Imk, newId, out bool allGood);
                             List<byte[]> oldIUKs = new List<byte[]>();
                             if(allGood)
                             {
@@ -84,31 +84,36 @@ namespace SQRLConsoleTester
                                 if(!string.IsNullOrEmpty(serverRespose.SIN))
                                 {
                                     additionalData = new StringBuilder();
-                                    byte[] ids = SQRL.CreateIndexedSecret(requestURI, AltID, decryptedData.Item2, Encoding.UTF8.GetBytes(serverRespose.SIN));
+                                    byte[] ids = SQRL.CreateIndexedSecret(requestURI, AltID, block1Keys.Imk, 
+                                        Encoding.UTF8.GetBytes(serverRespose.SIN));
                                     additionalData.AppendLineWindows($"ins={Sodium.Utilities.BinaryToBase64(ids, Utilities.Base64Variant.UrlSafeNoPadding)}");
                                 }
                                 Console.WriteLine("The site doesn't recognize this ID, would you like to proceed and create one? (Y/N)");
                                 if (Console.ReadLine().StartsWith("Y", StringComparison.OrdinalIgnoreCase))
                                 {
-                                    serverRespose = SQRL.GenerateNewIdentCommand(serverRespose.NewNutURL, siteKvp, serverRespose.FullServerRequest, decryptedData.Item3, opts, additionalData);
+                                    serverRespose = SQRL.GenerateNewIdentCommand(serverRespose.NewNutURL, siteKvp, 
+                                        serverRespose.FullServerRequest, block1Keys.Ilk, opts, additionalData);
                                 }
                             }
                             else if(serverRespose.PreviousIDMatch)
                             {
-                                
                                 byte[] ursKey = null;
-                                ursKey = SQRL.GetURSKey(serverRespose.PriorMatchedKey.Key, Sodium.Utilities.Base64ToBinary(serverRespose.SUK, string.Empty, Sodium.Utilities.Base64Variant.UrlSafeNoPadding));
+                                ursKey = SQRL.GetURSKey(serverRespose.PriorMatchedKey.Key, Sodium.Utilities.Base64ToBinary(
+                                    serverRespose.SUK, string.Empty, Sodium.Utilities.Base64Variant.UrlSafeNoPadding));
+
                                 StringBuilder additionalData = null;
                                 if (!string.IsNullOrEmpty(serverRespose.SIN))
                                 {
                                     additionalData = new StringBuilder();
-                                    byte[] ids = SQRL.CreateIndexedSecret(requestURI, AltID, decryptedData.Item2, Encoding.UTF8.GetBytes(serverRespose.SIN));
+                                    byte[] ids = SQRL.CreateIndexedSecret(requestURI, AltID, block1Keys.Imk, Encoding.UTF8.GetBytes(serverRespose.SIN));
                                     additionalData.AppendLineWindows($"ins={Sodium.Utilities.BinaryToBase64(ids, Utilities.Base64Variant.UrlSafeNoPadding)}");
                                     byte[] pids = SQRL.CreateIndexedSecret(serverRespose.PriorMatchedKey.Value.Item1, Encoding.UTF8.GetBytes(serverRespose.SIN));
                                     additionalData.AppendLineWindows($"pins={Sodium.Utilities.BinaryToBase64(pids, Utilities.Base64Variant.UrlSafeNoPadding)}");
                                     
                                 }
-                                serverRespose = SQRL.GenerateIdentCommandWithReplace(serverRespose.NewNutURL, siteKvp, serverRespose.FullServerRequest, decryptedData.Item3,ursKey,serverRespose.PriorMatchedKey.Value.Item2,opts, additionalData);
+                                serverRespose = SQRL.GenerateIdentCommandWithReplace(
+                                    serverRespose.NewNutURL, siteKvp, serverRespose.FullServerRequest, block1Keys.Ilk, 
+                                    ursKey,serverRespose.PriorMatchedKey.Value.Item2,opts, additionalData);
                             }
                             else if (serverRespose.CurrentIDMatch)
                             {
@@ -152,12 +157,12 @@ namespace SQRLConsoleTester
                                             Console.WriteLine($"Decrypting with Rescue Code: {percent.Key}%");
                                         });
                                         var iukData = await SQRL.DecryptBlock2(newId, rescueCode, progress);
-                                        if (iukData.Item1)
+                                        if (iukData.DecryptionSucceeded)
                                         {
                                             byte[] ursKey = null;
-                                            ursKey = SQRL.GetURSKey(iukData.Item2, Sodium.Utilities.Base64ToBinary(serverRespose.SUK, string.Empty, Sodium.Utilities.Base64Variant.UrlSafeNoPadding));
+                                            ursKey = SQRL.GetURSKey(iukData.Iuk, Sodium.Utilities.Base64ToBinary(serverRespose.SUK, string.Empty, Sodium.Utilities.Base64Variant.UrlSafeNoPadding));
                                             
-                                            iukData.Item2.ZeroFill();
+                                            iukData.Iuk.ZeroFill();
                                             serverRespose = SQRL.GenerateEnableCommand(serverRespose.NewNutURL, siteKvp,serverRespose.FullServerRequest, ursKey,addClientData, opts);
                                         }
                                         else
@@ -214,10 +219,10 @@ namespace SQRLConsoleTester
                                                 Console.WriteLine($"Decrypting with Rescue Code: {percent.Key}%");
                                             });
                                             var iukData = await SQRL.DecryptBlock2(newId, rescueCode);
-                                            if (iukData.Item1)
+                                            if (iukData.DecryptionSucceeded)
                                             {
-                                                byte[] ursKey = SQRL.GetURSKey(iukData.Item2, Sodium.Utilities.Base64ToBinary(serverRespose.SUK, string.Empty, Sodium.Utilities.Base64Variant.UrlSafeNoPadding));
-                                                iukData.Item2.ZeroFill();
+                                                byte[] ursKey = SQRL.GetURSKey(iukData.Iuk, Sodium.Utilities.Base64ToBinary(serverRespose.SUK, string.Empty, Sodium.Utilities.Base64Variant.UrlSafeNoPadding));
+                                                iukData.Iuk.ZeroFill();
                                                 serverRespose = SQRL.GenerateSQRLCommand(SQRLCommands.remove, serverRespose.NewNutURL, siteKvp, serverRespose.FullServerRequest, addClientData, opts,null,ursKey);
                                                 if (sqrl.cps != null && sqrl.cps.PendingResponse)
                                                 {
