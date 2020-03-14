890380146edef927a6e398596839924da46cfd87
Alexander Hauser <alex@hausers.cc>
2020-03-14 20:20:23 +0100

    Add NewPasswordView

---
+++ SQRLDotNetClientUI/Views/NewPasswordView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SQRLDotNetClientUI.Views
+{
+    public class NewPasswordView : UserControl
+    {
+        public NewPasswordView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
