3106738a1a1c3be5f6cd2b4e4620c98f62528f37
josegomez <jose@josecgomez.com>
2020-03-15 06:16:53 -0700

    Fixing MacOSX System Notification Bugs

--- SQRLDotNetClientUI/App.xaml.cs
+++ SQRLDotNetClientUI/App.xaml.cs
@@ -23,18 +23,20 @@ namespace SQRLDotNetClientUI
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                // Set up the app's main window
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
-
+              
                 // If this is running on a Mac we need a special event handler for URL schema Invokation
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 {
                     NSApplication.Init();
                     NSApplication.SharedApplication.Delegate = new  SQRLDotNetClientUI.Platform.OSX.AppDelegate((MainWindow)desktop.MainWindow);
                 }
+
+                  // Set up the app's main window
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+
                 
             }          
 
