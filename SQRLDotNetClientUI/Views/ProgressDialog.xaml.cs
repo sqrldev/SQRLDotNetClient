f1789c5a03fa8c71e22dc83f6d8a5aaccd11d28c
Alexander Hauser <alex@hausers.cc>
2020-03-17 07:07:14 +0100

    Add ProgressDialog

---
+++ SQRLDotNetClientUI/Views/ProgressDialog.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SQRLDotNetClientUI.Views
+{
+    public class ProgressDialog : Window
+    {
+        public ProgressDialog()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
