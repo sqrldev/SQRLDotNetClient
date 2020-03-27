cc76512fc962b18bcbf96eaf01c10388d38c1912
Alexander Hauser <alex@hausers.cc>
2020-03-27 11:31:29 +0100

    Implement manual language selection in MainMenuView and MainMenuViewModel

--- SQRLDotNetClientUI/ViewModels/MainMenuViewModel.cs
+++ SQRLDotNetClientUI/ViewModels/MainMenuViewModel.cs
@@ -4,11 +4,12 @@ using SQRLDotNetClientUI.Views;
 using SQRLUtilsLib;
 using System;
 using SQRLDotNetClientUI.Models;
-
 using Serilog;
-using System.Threading;
-using System.Linq;
 using Avalonia.Controls.ApplicationLifetimes;
+using System.Collections.Generic;
+using System.Windows.Input;
+using SQRLDotNetClientUI.AvaloniaExtensions;
+using Avalonia;
 
 namespace SQRLDotNetClientUI.ViewModels
 {
@@ -46,6 +47,43 @@ namespace SQRLDotNetClientUI.ViewModels
             set => this.RaiseAndSetIfChanged(ref _IdentityName, value);
         }
 
+        /// <summary>
+        /// Gets a list of menu item objects representing the different
+        /// languages that are supported by the app.
+        /// </summary>
+        public IReadOnlyList<LanguageMenuItem> LanguageMenuItems
+        {
+            get
+            {
+                List<LanguageMenuItem> items = new List<LanguageMenuItem>();
+                foreach (var locInfo in LocalizationExtension.Localizations)
+                {
+                    object logo;
+                    string prefix = string.Empty;
+
+                    if (LocalizationExtension.CurrentLocalization == locInfo.Key)
+                        logo = new CheckBox() { IsChecked = true, BorderThickness = new Thickness(0) };
+                    else
+                        logo = new Image() { Source = locInfo.Value.Image };
+
+                    if (locInfo.Key == LocalizationExtension.DEFAULT_LOC)
+                        prefix = _loc.GetLocalizationValue("DefaultLanguageMenuItemHeader") + " - ";
+
+                    LanguageMenuItem item = new LanguageMenuItem()
+                    {
+                        Header = prefix + locInfo.Value.CultureInfo.DisplayName,
+                        Command = ReactiveCommand.Create<string>(SelectLanguage),
+                        CommandParameter = locInfo.Key,
+                        Icon = logo
+                    };
+
+                    items.Add(item);
+                }
+                return items;
+            }
+            set { }
+        }
+
         public AuthenticationViewModel AuthVM { get; set; }
 
         public MainMenuViewModel()
@@ -79,6 +117,19 @@ namespace SQRLDotNetClientUI.ViewModels
         {
             this.IdentityName = e.IdentityName;
             this.CurrentIdentity = _identityManager.CurrentIdentity;
+        }
+
+        public void SelectLanguage(string language)
+        {
+            LocalizationExtension.CurrentLocalization = language;
+
+            //MainMenuView mmv = 
+            //MenuItem langMenu = mmv.FindControl<MenuItem>("menuLanguage");
+            //langMenu.Items = null;
+            //_mainWindow.DataContext = new MainWindowViewModel();
+
+            ((MainWindowViewModel)_mainWindow.DataContext).Content =
+                new MainMenuViewModel();
         }
 
         public void OnNewIdentityClick()
@@ -142,9 +193,10 @@ namespace SQRLDotNetClientUI.ViewModels
         {
 
             var result = await new Views.MessageBox(_loc.GetLocalizationValue("DeleteIdentityMessageBoxTitle"),
-                                                    string.Format(_loc.GetLocalizationValue("DeleteIdentityMessageBoxText"), this.IdentityName, Environment.NewLine),
-                                                    MessageBoxSize.Medium, MessageBoxButtons.YesNo, MessageBoxIcons.QUESTION)
-                                                    .ShowDialog<MessagBoxDialogResult>(_mainWindow);
+                string.Format(_loc.GetLocalizationValue("DeleteIdentityMessageBoxText"), this.IdentityName, Environment.NewLine),
+                MessageBoxSize.Medium, MessageBoxButtons.YesNo, MessageBoxIcons.QUESTION)
+                .ShowDialog<MessagBoxDialogResult>(_mainWindow);
+
             if (result == MessagBoxDialogResult.YES)
             {
                 _identityManager.DeleteCurrentIdentity();
@@ -176,5 +228,16 @@ namespace SQRLDotNetClientUI.ViewModels
             ((MainWindowViewModel)_mainWindow.DataContext).Content =
                 new ReKeyViewModel();
         }
+    }
+
+    /// <summary>
+    /// Represents a single menu item in the "languages" menu.
+    /// </summary>
+    public class LanguageMenuItem
+    {
+        public string Header;
+        public ICommand Command;
+        public object CommandParameter;
+        public object Icon;
     }
 }
--- SQRLDotNetClientUI/Views/MainMenuView.xaml.cs
+++ SQRLDotNetClientUI/Views/MainMenuView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using SQRLDotNetClientUI.AvaloniaExtensions;
+using SQRLDotNetClientUI.ViewModels;
+using System.Collections.Generic;
 
 namespace SQRLDotNetClientUI.Views
 {
@@ -9,16 +11,40 @@ namespace SQRLDotNetClientUI.Views
         public MainMenuView()
         {
             this.InitializeComponent();
-            
+            SetupLanguageMenu();
         }
 
-      
+        /// <summary>
+        /// Adds all available language options to the main menu.
+        /// </summary>
+        private void SetupLanguageMenu()
+        {
+            // We need to instantiate a LocalizationExtension here
+            // so that the can be sure that the localization data is initialized.
+            LocalizationExtension _ = new LocalizationExtension();
+
+            MenuItem languageMenu = this.FindControl<MenuItem>("menuLanguage");
+
+            List<MenuItem> items = new List<MenuItem>();
+            foreach (var lang in new MainMenuViewModel().LanguageMenuItems)
+            {
+                MenuItem item = new MenuItem()
+                {
+                    Header = lang.Header,
+                    Command = lang.Command,
+                    CommandParameter = lang.CommandParameter,
+                    Icon = lang.Icon
+                };
+
+                items.Add(item);
+            }
+
+            languageMenu.Items = items;
+        }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
-
-
     }
 }
