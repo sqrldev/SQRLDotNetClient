9b5a51fab26b5e5e814e49e70bda318e77daed33
Alexander Hauser <alex@hausers.cc>
2020-03-28 08:22:04 +0100

    Add NewPasswordWidget UserControl

---
+++ SQRLDotNetClientUI/Views/NewPasswordWidget.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SQRLDotNetClientUI.Views
+{
+    public class NewPasswordWidget : UserControl
+    {
+        public NewPasswordWidget()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
