42f44713c8a0810c189e9b4d915ff1b4e1a21ada
Alexander Hauser <alex@hausers.cc>
2020-03-29 09:21:37 +0200

    Implement NewPasswordWidget

--- SQRLDotNetClientUI/Views/NewPasswordWidget.xaml.cs
+++ SQRLDotNetClientUI/Views/NewPasswordWidget.xaml.cs
@@ -1,19 +1,133 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Threading;
+using SQRLDotNetClientUI.AvaloniaExtensions;
+using SQRLDotNetClientUI.Models;
+using System.Reactive.Linq;
+using System;
+using Avalonia.Controls.Shapes;
+using Avalonia.Data;
 
 namespace SQRLDotNetClientUI.Views
 {
     public class NewPasswordWidget : UserControl
     {
+        private static IBrush BRUSH_POOR = new SolidColorBrush(new Color(0xFF, 0xF0, 0x80, 0x80));
+        private static IBrush BRUSH_MEDIUM = new SolidColorBrush(new Color(0xFF, 0xFF, 0xFA, 0xCD));
+        private static IBrush BRUSH_GOOD = new SolidColorBrush(new Color(0xFF, 0x32, 0xCD, 0x32));
+
+        private LocalizationExtension _loc = new LocalizationExtension();
+        private PasswordStrengthMeter _pwdStrengthMeter = new PasswordStrengthMeter();
+        private CopyPasteTextBox _txtNewPassword = null;
+        private CopyPasteTextBox _txtNewPasswordVerify = null;
+        private Panel _pnlPasswordRating = null;
+        private ProgressBar _progressPwStrength = null;
+        private TextBlock _lblPasswordStrength = null;
+        private Ellipse _shapeUppercaseIndicator = null;
+        private Ellipse _shapeLowercaseIndicator = null;
+        private Ellipse _shapeDigigsIndicator = null;
+        private Ellipse _shapeSymbolsIndicator = null;
+
+        public static readonly AvaloniaProperty<bool> PasswordsMatchProperty =
+            AvaloniaProperty.Register<NewPasswordWidget, bool>("PasswordsMatch", defaultValue: false, 
+                defaultBindingMode: BindingMode.TwoWay);
+
+        /// <summary>
+        /// Indicates whether the entered password and the password verification 
+        /// are equal.
+        /// </summary>
+        public bool PasswordsMatch
+        {
+            get { return this.GetValue(PasswordsMatchProperty); }
+            set { this.SetValue(PasswordsMatchProperty, value); }
+        }
+
         public NewPasswordWidget()
         {
             this.InitializeComponent();
+
+            _pwdStrengthMeter.ScoreUpdated += PasswordStrengthScoreUpdated;
+            _txtNewPassword = this.FindControl<CopyPasteTextBox>("txtNewPassword");
+            _txtNewPasswordVerify = this.FindControl<CopyPasteTextBox>("txtNewPasswordVerify");
+            _pnlPasswordRating = this.FindControl<Panel>("pnlPasswordRating");
+            _progressPwStrength = this.FindControl<ProgressBar>("progressPwStrength");
+            _lblPasswordStrength = this.FindControl<TextBlock>("lblPasswordStrength");
+            _shapeUppercaseIndicator = this.FindControl<Ellipse>("shapeUppercaseIndicator");
+            _shapeLowercaseIndicator = this.FindControl<Ellipse>("shapeLowercaseIndicator");
+            _shapeDigigsIndicator = this.FindControl<Ellipse>("shapeDigigsIndicator");
+            _shapeSymbolsIndicator = this.FindControl<Ellipse>("shapeSymbolsIndicator");
+
+            _txtNewPassword.GetObservable(CopyPasteTextBox.TextProperty).Subscribe(value =>
+            {
+                _pwdStrengthMeter.Update(value);
+                CheckPasswordVerification();
+            });
+
+            _txtNewPasswordVerify.GetObservable(CopyPasteTextBox.TextProperty).Subscribe(value =>
+            {
+                CheckPasswordVerification();
+            });
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        /// <summary>
+        /// Checks if the password verification matches the new password
+        /// and enables/disables UI controls accordingly.
+        /// </summary>
+        private void CheckPasswordVerification()
+        {
+            if (!string.IsNullOrEmpty(_txtNewPassword.Text) && 
+                _txtNewPassword.Text == _txtNewPasswordVerify.Text)
+            {
+                this.PasswordsMatch = true;
+            }
+            else this.PasswordsMatch = false;
+        }
+
+        /// <summary>
+        /// Gets called if the <c>PasswordStrengthMeter</c> has calculated a new
+        /// password strength rating.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PasswordStrengthScoreUpdated(object sender, ScoreUpdatedEventArgs e)
+        {
+            Dispatcher.UIThread.Post(() =>
+            {
+                string ratingText = _loc.GetLocalizationValue("PasswordStrength") + ": ";
+
+                switch (e.Score.Rating)
+                {
+                    case PasswordRating.POOR:
+                        ratingText += _loc.GetLocalizationValue("PasswordStrengthRatingPoor");
+                        _pnlPasswordRating.Background = BRUSH_POOR;
+                        break;
+
+                    case PasswordRating.MEDIUM:
+                        ratingText += _loc.GetLocalizationValue("PasswordStrengthRatingMedium");
+                        _pnlPasswordRating.Background = BRUSH_MEDIUM;
+                        break;
+
+                    case PasswordRating.GOOD:
+                        ratingText += _loc.GetLocalizationValue("PasswordStrengthRatingGood");
+                        _pnlPasswordRating.Background = BRUSH_GOOD;
+                        break;
+                }
+
+                _shapeUppercaseIndicator.Fill = e.Score.UppercaseUsed ? BRUSH_GOOD : BRUSH_POOR;
+                _shapeLowercaseIndicator.Fill = e.Score.LowercaseUsed ? BRUSH_GOOD : BRUSH_POOR;
+                _shapeDigigsIndicator.Fill = e.Score.DigitsUsed ? BRUSH_GOOD : BRUSH_POOR;
+                _shapeSymbolsIndicator.Fill = e.Score.SymbolsUsed ? BRUSH_GOOD : BRUSH_POOR;
+
+                _lblPasswordStrength.Text = ratingText;
+                _progressPwStrength.Value = (double)e.Score.StrengthPoints;
+            });
         }
     }
 }
