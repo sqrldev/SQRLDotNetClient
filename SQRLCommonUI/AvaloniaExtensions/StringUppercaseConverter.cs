5dba362baa260df4db84f55e05256912141c0f6d
Alexander Hauser <alex@hausers.cc>
2020-04-04 17:40:03 +0200

    Fix class access for StringUppercaseConverter

--- SQRLCommonUI/AvaloniaExtensions/StringUppercaseConverter.cs
+++ SQRLCommonUI/AvaloniaExtensions/StringUppercaseConverter.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace SQRLCommonUI.AvaloniaExtensions
 {
-    class StringUppercaseConverter : IValueConverter
+    public class StringUppercaseConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
