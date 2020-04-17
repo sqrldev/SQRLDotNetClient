78571293973f59dea8042ede0552a1e7256ea82b
Alexander Hauser <alex@hausers.cc>
2020-04-17 17:08:12 +0200

    Add documentation to ProgressDialogViewModel

--- SQRLDotNetClientUI/ViewModels/ProgressDialogViewModel.cs
+++ SQRLDotNetClientUI/ViewModels/ProgressDialogViewModel.cs
@@ -4,42 +4,93 @@ using System.Text;
 
 namespace SQRLDotNetClientUI.ViewModels
 {
+    /// <summary>
+    /// A view model representing the app's "progress dialog" screen.
+    /// </summary>
     public class ProgressDialogViewModel: ViewModelBase
     {
+        /// <summary>
+        /// Gets or sets a list of <c>Progress</c> objects being tracked by the progress dialog.
+        /// </summary>
         public List<Progress<KeyValuePair<int, string>>> ProgressList { get; set; }
+
+        /// <summary>
+        /// Gets or sets the view model from which the progress dialog was called.
+        /// This view model will be returned to after the progress dialog closes.
+        /// </summary>
         public ViewModelBase Parent { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether progress items which are finished
+        /// should be hidden from the dialog or not.
+        /// </summary>
         public bool HideFinishedItems { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether progress items that have not yet
+        /// received any progress events should be hidden or not.
+        /// </summary>
         public bool HideEnqueuedItems { get; set; }
 
-        
 
+        /// <summary>
+        /// Creates a new <c>ProgressDialogViewModel</c> instance.
+        /// </summary>
         private ProgressDialogViewModel()
         {
             
         }
-        private ProgressDialogViewModel(ViewModelBase model, bool HideFinishedItems = true, bool HideEnqueuedItems = true) :this()
+
+        /// <summary>
+        /// Creates a new <c>ProgressDialogViewModel</c> instance.
+        /// </summary>
+        /// <param name="parent">The view model calling the progress dialog.</param>
+        /// <param name="hideFinishedItems">Specifies whether progress items which are finished
+        /// should be hidden from the dialog or not.</param>
+        /// <param name="hideEnqueuedItems">Specifies whether progress items that have not yet
+        /// received any progress events should be hidden or not.</param>
+        private ProgressDialogViewModel(ViewModelBase parent, bool hideFinishedItems = true, bool hideEnqueuedItems = true) : this()
         {
-            this.Parent = model;
+            this.Parent = parent;
             this.Title = this.Parent.Title;
-            this.HideFinishedItems = HideFinishedItems;
-            this.HideEnqueuedItems = HideEnqueuedItems;
+            this.HideFinishedItems = hideFinishedItems;
+            this.HideEnqueuedItems = hideEnqueuedItems;
         }
 
-        public ProgressDialogViewModel(List<Progress<KeyValuePair<int, string>>> progressList, ViewModelBase model, bool HideFinishedItems = true, bool HideEnqueuedItems = true) :this(model, HideFinishedItems, HideEnqueuedItems)
+        /// <summary>
+        /// Creates a new <c>ProgressDialogViewModel</c> instance.
+        /// </summary>
+        /// <param name="progressList">A list of <c>Progress</c> objects to be tracked by the progress dialog.</param>
+        /// <param name="parent">The view model calling the progress dialog.</param>
+        /// <param name="hideFinishedItems">Specifies whether progress items which are finished
+        /// should be hidden from the dialog or not.</param>
+        /// <param name="hideEnqueuedItems">Specifies whether progress items that have not yet
+        /// received any progress events should be hidden or not.</param>
+        public ProgressDialogViewModel(List<Progress<KeyValuePair<int, string>>> progressList, ViewModelBase parent, 
+            bool HideFinishedItems = true, bool HideEnqueuedItems = true) : this(parent, HideFinishedItems, HideEnqueuedItems)
         {
             this.ProgressList = progressList;
 
         }
 
-        public ProgressDialogViewModel(Progress<KeyValuePair<int, string>> progress, ViewModelBase model, bool HideFinishedItems = false, bool HideEnqueuedItems = false) : this (model, HideFinishedItems, HideEnqueuedItems)
+        /// <summary>
+        /// Creates a new <c>ProgressDialogViewModel</c> instance.
+        /// </summary>
+        /// <param name="progress">A <c>Progress</c> object to be tracked by the progress dialog.</param>
+        /// <param name="parent">The view model calling the progress dialog.</param>
+        /// <param name="hideFinishedItems">Specifies whether progress items which are finished
+        /// should be hidden from the dialog or not.</param>
+        /// <param name="hideEnqueuedItems">Specifies whether progress items that have not yet
+        /// received any progress events should be hidden or not.</param>
+        public ProgressDialogViewModel(Progress<KeyValuePair<int, string>> progress, ViewModelBase parent, 
+            bool HideFinishedItems = false, bool HideEnqueuedItems = false) : this (parent, HideFinishedItems, HideEnqueuedItems)
         {
             this.ProgressList = new List<Progress<KeyValuePair<int, string>>>();
             this.ProgressList.Add(progress);
         }
 
         /// <summary>
-        /// Sets the current progress dialog as the forground User Control
+        /// Sets the current progress dialog as the foreground <c>UserControl</c>.
         /// </summary>
         public void ShowDialog()
         {
@@ -48,7 +99,7 @@ namespace SQRLDotNetClientUI.ViewModels
 
 
         /// <summary>
-        /// Sets the Parent of the Progress Dialog back to the Foreground control.
+        /// Sets the parent of the progress dialog as the foreground <c>UserControl</c>.
         /// </summary>
         public void Close()
         {
