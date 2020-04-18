cbeea842063deab9b7bfe10c13054b50306cfc98
Alexander Hauser <alex@hausers.cc>
2020-04-18 18:52:27 +0200

    Add AppSettings class

---
+++ SQRLDotNetClientUI/Models/AppSettings.cs
@@ -0,0 +1,95 @@
+﻿using SQRLDotNetClientUI.DB.DBContext;
+using SQRLDotNetClientUI.DB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQRLDotNetClientUI.Models
+{
+    /// <summary>
+    /// This class exposes and manages application and user settings.
+    /// </summary>
+    public class AppSettings
+    {
+        private static AppSettings _instance = null;
+        private SQRLDBContext _db = null;
+        private UserData _userData = null;
+
+        /// <summary>
+        /// Gets the singleton <c>AppSettings</c> instance.
+        /// </summary>
+        public static AppSettings Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new AppSettings();
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// The id of the last loaded identity.
+        /// </summary>
+        public string LastLoadedIdentity 
+        { 
+            get 
+            { 
+                return _userData.LastLoadedIdentity; 
+            }
+            set
+            {
+                _userData.LastLoadedIdentity = value;
+                _db.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value that determines if the app should start 
+        /// minimized to the tray icon.
+        /// </summary>
+        public bool StartMinimized 
+        {
+            get
+            {
+                return _userData.StartMinimized;
+            }
+            set
+            {
+                _userData.StartMinimized = value;
+                _db.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// The constructor is private, use the <Instance>property</Instance>
+        /// to get the singletion class instance.
+        /// </summary>
+        private AppSettings()
+        {
+            _db = new SQRLDBContext();
+            _userData = GetUserData();
+        }
+
+        /// <summary>
+        /// Returns the <c>UserData</c> database entry. If no such entry
+        /// exists, it will be created.
+        /// </summary>
+        private UserData GetUserData()
+        {
+            UserData result = null;
+
+            result = _db.UserData.FirstOrDefault();
+            if (result == null)
+            {
+                UserData ud = new UserData();
+                ud.LastLoadedIdentity = string.Empty;
+                _db.UserData.Add(ud);
+                _db.SaveChanges();
+                result = ud;
+            }
+
+            return result;
+        }
+    }
+}
