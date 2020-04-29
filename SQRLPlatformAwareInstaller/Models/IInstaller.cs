ba23c803b76264d738886887e56448ab7a9c81fe
Alexander Hauser <alex@hausers.cc>
2020-04-29 21:55:57 +0200

    Update IInstaller interface

--- SQRLPlatformAwareInstaller/Models/IInstaller.cs
+++ SQRLPlatformAwareInstaller/Models/IInstaller.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace SQRLPlatformAwareInstaller.Models
 {
     /// <summary>
-    /// Represents an interface for the actual client installation.
+    /// An interface for performing the actual client installation/uninstallation.
     /// 
     /// Platform-specific implementations for this interface can be found in 
     /// <c>SQRLPlatformAwareInstaller.Platform.XXX</c>.
@@ -18,13 +18,22 @@ namespace SQRLPlatformAwareInstaller.Models
         /// </summary>
         /// <param name="archiveFilePath">The full file name, including the path,
         /// of the archive containing the components to install.</param>
-        public void Install(string archiveFilePath);
+        /// <param name="installPath">The full path to the client installation directory.</param>
+        /// <param name="progress">An object for receiving installation progress updates.</param>
+        public void Install(string archiveFilePath, string installPath, IProgress<int> progress);
 
         /// <summary>
-        /// Performs the actual installation if the client app.
+        /// Performs the uninstallation if the client app.
         /// </summary>
         /// <param name="uninstallInfoFile">The full file name, including the path,
         /// of the file containing the uninstall information.</param>
         public void UnInstall(string uninstallInfoFile);
+
+        /// <summary>
+        /// Returns the full path to the client executable using the given
+        /// <paramref name="installPath"/>.
+        /// </summary>
+        /// <param name="installPath">The client installation path.</param>
+        public string GetExecutablePath(string installPath);
     }
 }
