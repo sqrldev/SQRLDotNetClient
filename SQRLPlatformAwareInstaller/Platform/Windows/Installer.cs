25e1f05b1258eb7a7c740bf80ddeb8b1d0ff7bf0
Alexander Hauser <alex@hausers.cc>
2020-04-29 20:02:05 +0200

    Add installer stub for Windows

---
+++ SQRLPlatformAwareInstaller/Platform/Windows/Installer.cs
@@ -0,0 +1,20 @@
+﻿using SQRLPlatformAwareInstaller.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQRLPlatformAwareInstaller.Platform.Windows
+{
+    class Installer : IInstaller
+    {
+        public void Install(string archiveFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnInstall(string uninstallInfoFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
