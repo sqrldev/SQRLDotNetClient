a9a59448702456cf82fc9412256cfed100951c7f
Alexander Hauser <alex@hausers.cc>
2020-05-02 10:54:40 +0200

    Update UninstallView/UninstallViewModel

--- SQRLPlatformAwareInstaller/ViewModels/UninstallViewModel.cs
+++ SQRLPlatformAwareInstaller/ViewModels/UninstallViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Threading;
+﻿using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Threading;
 using ReactiveUI;
 using SQRLPlatformAwareInstaller.Models;
 using System;
@@ -11,7 +12,10 @@ namespace SQRLPlatformAwareInstaller.ViewModels
     public class UninstallViewModel : ViewModelBase
     {
         private string _uninstallLog = "";
+        private string _uninstallButtonText = "";
         private decimal _progressPercentage = 0;
+        private bool _canUninstall = true;
+        private bool _uninstallFinished = false;
 
         /// <summary>
         /// Gets or sets the uninstall log text.
@@ -20,6 +24,15 @@ namespace SQRLPlatformAwareInstaller.ViewModels
         {
             get { return this._uninstallLog; }
             set { this.RaiseAndSetIfChanged(ref this._uninstallLog, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the uninstall button text.
+        /// </summary>
+        public string UninstallButtonText
+        {
+            get { return this._uninstallButtonText; }
+            set { this.RaiseAndSetIfChanged(ref this._uninstallButtonText, value); }
         }
 
         /// <summary>
@@ -32,11 +45,37 @@ namespace SQRLPlatformAwareInstaller.ViewModels
         }
 
         /// <summary>
+        /// Gets or sets a value indicating whether the uninstall button should be enabled.
+        /// </summary>
+        public bool CanUninstall
+        {
+            get { return this._canUninstall; }
+            set { this.RaiseAndSetIfChanged(ref this._canUninstall, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the uninstall process has finished.
+        /// </summary>
+        public bool UninstallFinished
+        {
+            get { return this._uninstallFinished; }
+            set 
+            { 
+                this.UninstallButtonText = value ?
+                    _loc.GetLocalizationValue("BtnFinish") :
+                    _loc.GetLocalizationValue("BtnUninstall");
+
+                this.RaiseAndSetIfChanged(ref this._uninstallFinished, value); 
+            }
+        }
+
+        /// <summary>
         /// Creates a new instance and performs some initialization tasks.
         /// </summary>
         public UninstallViewModel()
         {
             this.Title = _loc.GetLocalizationValue("TitleUninstall");
+            this.UninstallButtonText = _loc.GetLocalizationValue("BtnUninstall");
         }
 
         /// <summary>
@@ -44,6 +83,11 @@ namespace SQRLPlatformAwareInstaller.ViewModels
         /// </summary>
         public async void Uninstall()
         {
+            // If "Uninstall()" was already run before, this method gets called again when 
+            // the user hits the "finish" button. If that's the case, just drop out. 
+            if (this.UninstallFinished) Cancel();
+            
+            // If we get here, we're about to launch the actual uninstall
             Progress<Tuple<int, string>> progress = new Progress<Tuple<int, string>>((x) =>
             {
                 Dispatcher.UIThread.Post(() =>
@@ -53,7 +97,19 @@ namespace SQRLPlatformAwareInstaller.ViewModels
                 });
             });
 
+            this.CanUninstall = false;
             await Uninstaller.Run(progress, dryRun: false);
+            this.UninstallFinished = true;
+            this.CanUninstall = true;
+        }
+
+        /// <summary>
+        /// Closes the installer.
+        /// </summary>
+        public void Cancel()
+        {
+            (App.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)
+                .Shutdown();
         }
     }
 }
