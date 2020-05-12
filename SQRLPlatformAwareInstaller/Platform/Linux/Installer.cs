52d6ffcb50ab63f6ffbaa99a3dc0a859066eb36e
Alexander Hauser <alex@hausers.cc>
2020-05-12 16:38:07 +0200

    Update installer for Linux to support throwing exceptions; Remove copying of installer binary

--- SQRLPlatformAwareInstaller/Platform/Linux/Installer.cs
+++ SQRLPlatformAwareInstaller/Platform/Linux/Installer.cs
@@ -17,22 +17,22 @@ namespace SQRLPlatformAwareInstaller.Platform.Linux
         private static IBridgeSystem _bridgeSystem { get; set; } = BridgeSystem.Bash;
         private static ShellConfigurator _shell { get; set; } = new ShellConfigurator(_bridgeSystem);
 
+        #pragma warning disable 1998
         public async Task Install(string archiveFilePath, string installPath, string versionTag)
         {
-            Log.Information("Installing on Linux");
-            Inventory.Instance.Load();
-
-            if (!Directory.Exists(installPath))
-            {
-                Directory.CreateDirectory(installPath);
-            }
-
+            // The "async" in the delegate is needed, otherwise exceptions within
+            // the delegate won't "bubble up" to the exception handlers upstream.
             await Task.Run(() =>
             {
+                Inventory.Instance.Load();
+                Log.Information($"Installing on Linux to {installPath}");
+
+                // Extract main installation archive
                 Log.Information($"Extracting main installation archive");
                 Utils.ExtractZipFile(archiveFilePath, string.Empty, installPath);
 
-                //If the DB Exists in the current install path
+                // Check if a database exists in the installation directory 
+                // (which is bad) and if it does, move it to user space.
                 if (File.Exists(Path.Combine(installPath, PathConf.DBNAME)))
                 {
                     Utils.MoveDb(Path.Combine(installPath, PathConf.DBNAME));
@@ -40,48 +40,35 @@ namespace SQRLPlatformAwareInstaller.Platform.Linux
                 
                 Inventory.Instance.AddDirectory(installPath);
 
-                Log.Information("Copying installer into installation location (for auto update)");
-                try
-                {
-                    //Copy the installer but don't over-write the one included in the zip since it will likely be newer
-                    File.Copy(Process.GetCurrentProcess().MainModule.FileName, 
-                        Path.Combine(installPath, Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName)), false);
-                }
-                catch (Exception fc)
-                {
-                    Log.Warning($"File copy exception while copying installer:\r\n{fc}");
-                }
-            });
-
-            Log.Information("Creating Linux desktop icon, application and registering SQRL invokation scheme");
-            await Task.Run(() =>
-            {
+                // Create icon, register sqrl:// scheme etc.
+                Log.Information("Creating Linux desktop icon, application and registering SQRL invokation scheme");
                 GitHubHelper.DownloadFile(@"https://github.com/sqrldev/SQRLDotNetClient/raw/master/SQRLDotNetClientUI/Assets/SQRL_icon_normal_64.png",
-                    Path.Combine(installPath, "SQRL.png"));
+                        Path.Combine(installPath, "SQRL.png"));
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"[Desktop Entry]");
+                sb.AppendLine("Name=SQRL");
+                sb.AppendLine("Type=Application");
+                sb.AppendLine($"Icon={(Path.Combine(installPath, "SQRL.png"))}");
+                sb.AppendLine($"Exec={GetClientExePath(installPath)} %u");
+                sb.AppendLine("Categories=Internet");
+                sb.AppendLine("Terminal=false");
+                sb.AppendLine("MimeType=x-scheme-handler/sqrl");
+                File.WriteAllText(Path.Combine(installPath, "sqrldev-sqrl.desktop"), sb.ToString());
+
+                _shell.Term($"chmod -R 755 {installPath}", Output.Internal);
+                _shell.Term($"chmod a+x {GetClientExePath(installPath)}", Output.Internal);
+                _shell.Term($"chmod +x {Path.Combine(installPath, "sqrldev-sqrl.desktop")}", Output.Internal);
+                _shell.Term($"chmod a+x {Path.Combine(installPath, Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName))}", Output.Internal);
+                _shell.Term($"xdg-desktop-menu install {Path.Combine(installPath, "sqrldev-sqrl.desktop")}", Output.Internal);
+                _shell.Term($"gio mime x-scheme-handler/sqrl sqrldev-sqrl.desktop", Output.Internal);
+                _shell.Term($"xdg-mime default sqrldev-sqrl.desktop x-scheme-handler/sqrl", Output.Internal);
+                _shell.Term($"update-desktop-database ~/.local/share/applications/", Output.Internal);
+
+                Inventory.Instance.Save();
             });
-
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"[Desktop Entry]");
-            sb.AppendLine("Name=SQRL");
-            sb.AppendLine("Type=Application");
-            sb.AppendLine($"Icon={(Path.Combine(installPath, "SQRL.png"))}");
-            sb.AppendLine($"Exec={GetClientExePath(installPath)} %u");
-            sb.AppendLine("Categories=Internet");
-            sb.AppendLine("Terminal=false");
-            sb.AppendLine("MimeType=x-scheme-handler/sqrl");
-            File.WriteAllText(Path.Combine(installPath, "sqrldev-sqrl.desktop"), sb.ToString());
-
-            _shell.Term($"chmod -R 755 {installPath}", Output.Internal);
-            _shell.Term($"chmod a+x {GetClientExePath(installPath)}", Output.Internal);
-            _shell.Term($"chmod +x {Path.Combine(installPath, "sqrldev-sqrl.desktop")}", Output.Internal);
-            _shell.Term($"chmod a+x {Path.Combine(installPath, Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName))}", Output.Internal);
-            _shell.Term($"xdg-desktop-menu install {Path.Combine(installPath, "sqrldev-sqrl.desktop")}", Output.Internal);
-            _shell.Term($"gio mime x-scheme-handler/sqrl sqrldev-sqrl.desktop", Output.Internal);
-            _shell.Term($"xdg-mime default sqrldev-sqrl.desktop x-scheme-handler/sqrl", Output.Internal);
-            _shell.Term($"update-desktop-database ~/.local/share/applications/", Output.Internal);
-
-            Inventory.Instance.Save();
         }
+        #pragma warning restore 1998
 
         public async Task Uninstall(IProgress<Tuple<int, string>> progress = null, bool dryRun = true)
         {
