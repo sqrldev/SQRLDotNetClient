b6e2fe6cb93a28e83385f0dc57c6d35266396f64
Alexander Hauser <alex@hausers.cc>
2020-05-21 09:39:47 +0200

    Consolidate CommandLineArgs
    Remove InstallerCommands class and move Instance property into CommandLineArgs. No reason for having two separate classes here.

--- SQRLPlatformAwareInstaller/Models/CommandLineArgs.cs
+++ SQRLPlatformAwareInstaller/Models/CommandLineArgs.cs
@@ -6,8 +6,9 @@ using CommandLine;
 namespace SQRLPlatformAwareInstaller.Models
 {
     /// <summary>
-    /// This enum is used by the installer to determine the action that was requested to be performed
-    /// by default it would be Install, but we also have the option to Uninstall and eventually upgrade the application.
+    /// This enum is used by the installer to determine the action that was requested to be performed.
+    /// By default, it would be "Install", but we also have the option to "Uninstall" and eventually 
+    /// "Update" the application.
     /// </summary>
     public enum InstallerAction
     {
@@ -17,44 +18,61 @@ namespace SQRLPlatformAwareInstaller.Models
     }
 
     /// <summary>
-    /// This class holds all our allowed command line switches
+    /// This class holds all our allowed command line switches.
     /// </summary>
     public class CommandLineArgs
     {
         /// <summary>
-        /// Command line switch -a | -action <see cref="InstallerAction"/>
+        /// Command line switch -a | -action. <see cref="InstallerAction"/>
         /// </summary>
-        [Option('a',"action",Required =false, Default = InstallerAction.Install, HelpText = "Specifies the action to be performed (Install, Uninstall, Update).")]
+        [Option('a', "action", Required = false, Default = InstallerAction.Install, 
+            HelpText = "Specifies the action to be performed (Install, Uninstall, Update).")]
         public InstallerAction Action { get; set; }
 
         /// <summary>
-        /// Command line switch -p | -path "path" Specifies SQRL's current Install path (directory)
+        /// Command line switch -p | -path "path". Specifies SQRL's current install path (directory).
         /// </summary>
-        [Option('p', "path", Required = false,  Default ="", HelpText = "Specifies SQRL's current Install path (directory)")]
+        [Option('p', "path", Required = false,  Default = "", 
+            HelpText = "Specifies SQRL's current Install path (directory)")]
         public string InstallPath { get; set; }
 
         /// <summary>
-        /// Command line switch -z | zip "path" Specifies the location of the temporary update zip file (used in conjunction with update action)
+        /// Command line switch -z | zip "path". Specifies the location of the temporary 
+        /// update zip file (used in conjunction with update action)
         /// </summary>
-        [Option('z', "zip", Required = false, Default = "", HelpText = "Specifies the location of the temporary update zip file (used in conjunction with update action)")]
-        public string TempAzipPath { get; set; }
+        [Option('z', "zip", Required = false, Default = "", 
+            HelpText = "Specifies the location of the temporary update zip file (used in conjunction with update action)")]
+        public string ZipFilePath { get; set; }
 
-        public override string ToString()
+        /// <summary>
+        /// Provides static access to the command line arguments passed to the
+        /// currently running application.
+        /// </summary>
+        public static CommandLineArgs Instance
         {
-            return $"-a {this.Action} -p: {this.InstallPath} -z: {this.TempAzipPath}";
-        }
-    }
-
-    public static class InstallerCommands
-    {
-        public static SQRLPlatformAwareInstaller.Models.CommandLineArgs Instance {
-            get{
+            get
+            {
                 string[] args = Environment.GetCommandLineArgs();
                 var r = Parser.Default.ParseArguments<CommandLineArgs>(args);
                 if (r.Tag == ParserResultType.Parsed)
-                    return ((CommandLine.Parsed<SQRLPlatformAwareInstaller.Models.CommandLineArgs>)r).Value;
+                {
+                    return ((Parsed<CommandLineArgs>)r).Value;
+                }
                 else
-                    return null;  
-            } }
+                {
+                    return new CommandLineArgs()
+                    {
+                        Action = InstallerAction.Install,
+                        InstallPath = null,
+                        ZipFilePath = null
+                    };
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"-a {this.Action} -p: {this.InstallPath} -z: {this.ZipFilePath}";
+        }
     }
 }
