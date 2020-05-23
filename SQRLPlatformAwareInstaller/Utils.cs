4cae30e67893f635c143e86235e2ad4487fb69e3
Alexander Hauser <alex@hausers.cc>
2020-05-23 08:21:31 +0200

    Add ExtractSingleFile() to Utils class

--- SQRLPlatformAwareInstaller/Utils.cs
+++ SQRLPlatformAwareInstaller/Utils.cs
@@ -18,6 +18,7 @@ namespace SQRLPlatformAwareInstaller
 
         private static IBridgeSystem _bridgeSystem { get; set; } = BridgeSystem.Bash;
         private static ShellConfigurator _shell { get; set; } = new ShellConfigurator(_bridgeSystem);
+
         /// <summary>
         /// Extracts the zip archive specified by <paramref name="archivePath"/> into the
         /// output directory <paramref name="outFolder"/> using <paramref name="password"/>.
@@ -98,10 +99,58 @@ namespace SQRLPlatformAwareInstaller
         }
 
         /// <summary>
-        /// Grants full access permissions for <paramref name="file"/> to the current user.
+        /// Extracts a single file specified by <paramref name="fileName"/> from the zip archive 
+        /// specified by <paramref name="archivePath"/>, optionally using the specified <paramref name="password"/>,
+        /// to the output file specified by <paramref name="outputFilePath"/>.
         /// </summary>
-        /// <param name="file">The file to set the permissions for.</param>
-        public static void GrantFullFileAccess(string file)
+        /// <param name="archivePath">The full path to the archive to extract the file from.</param>
+        /// <param name="password">The password for the archive. Can be <c>null</c>.</param>
+        /// <param name="fileName">The file name (or path) within the archive of the file to extract.</param>
+        /// <param name="outputFilePath">The full destination path for the extracted file.</param>
+        public static void ExtractSingleFile(string archivePath, string password, string fileName, string outputFilePath)
+        {
+            using (Stream inputStream = File.OpenRead(archivePath))
+            {
+                using (var zipFile = new ZipFile(inputStream))
+                {
+                    //We don't password protect our install but maybe we should
+                    if (!String.IsNullOrEmpty(password))
+                    {
+                        // AES encrypted entries are handled automatically
+                        zipFile.Password = password;
+                    }
+
+                    ZipEntry zipEntry = zipFile.GetEntry(fileName);
+
+                    // Check if the output directory exists and create it if necessary
+                    var directoryName = Path.GetDirectoryName(outputFilePath);
+                    if (!string.IsNullOrEmpty(directoryName) && !Directory.Exists(directoryName))
+                    {
+                        Directory.CreateDirectory(directoryName);
+                        GrantFullFileAccess(directoryName);
+                    }
+
+                    // 4K is optimum
+                    var buffer = new byte[4096];
+
+                    // Unzip file in buffered chunks. This is just as fast as unpacking
+                    // to a buffer the full size of the file, but does not waste memory.
+                    // The "using" will close the stream even if an exception occurs.
+                    using (var zipStream = zipFile.GetInputStream(zipEntry))
+                    using (Stream fsOutput = File.Create(outputFilePath))
+                    {
+                        StreamUtils.Copy(zipStream, fsOutput, buffer);
+                        GrantFullFileAccess(outputFilePath);
+                    }
+                }
+            }
+        }
+
+            /// <summary>
+            /// Grants full access permissions for <paramref name="file"/> to the current user.
+            /// </summary>
+            /// <param name="file">The file to set the permissions for.</param>
+            public static void GrantFullFileAccess(string file)
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
