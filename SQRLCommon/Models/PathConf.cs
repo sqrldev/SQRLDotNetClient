759865a0504d6e5707e2340c691da5d009733d9b
Alexander Hauser <alex@hausers.cc>
2020-05-27 10:33:51 +0200

    Update PathConf to support handling logging path

--- SQRLCommon/Models/PathConf.cs
+++ SQRLCommon/Models/PathConf.cs
@@ -65,20 +65,28 @@ namespace SQRLCommon.Models
             get
             {
                 string defaultDbPath = "";
-                /*
-                * If this is running on linux we and running as admin we need to hack our way
-                * to find the current user's home directory so that we get the correct Default DB path
-                */
+
+                // If this is running on linux we and running as admin we need to hack our way
+                // to find the current user's home directory so that we get the correct Default DB path
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && SystemAndShellUtils.IsAdmin())
                 {
                     defaultDbPath = Path.Combine(SystemAndShellUtils.GetHomePath(), "SQRL");
                 }
                 else
+                {
                     defaultDbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SQRL");
+                }
 
                 return defaultDbPath;
             }
         }
+
+        /// <summary>
+        /// The default client installation directory.
+        /// </summary>
+        public static readonly string DefaultLogPath = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ?
+            Path.Combine("/var/log", "SQRL") :
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SQRL", "Logs");
 
         /// <summary>
         /// Gets or sets the SQRL client installation directory path.
@@ -115,6 +123,23 @@ namespace SQRLCommon.Models
         }
 
         /// <summary>
+        /// Gets or sets the SQRL client's database directory path.
+        /// </summary>
+        public static string LogPath
+        {
+            get
+            {
+                LoadConfig();
+                return _model.LogPath;
+            }
+            set
+            {
+                _model.LogPath = value;
+                SaveConfig();
+            }
+        }
+
+        /// <summary>
         /// The full file path, including the file name, to the client database.
         /// </summary>
         public static string FullClientDbPath
@@ -140,35 +165,50 @@ namespace SQRLCommon.Models
         /// </summary>
         public static void LoadConfig()
         {
-            Log.Information($"Config File: {ConfFile}");
-            if (!File.Exists(ConfFile))
-            {
+            // In no config file exists, "loading" shall reset the config to 
+            // default values. We achieve this by simply creating a new model.
+            _model = new PathConfModel();
 
-                // In no config file exists, "loading" shall reset the config to 
-                // default values. We achieve this by simply creating a new model.
-                _model = new PathConfModel();
+            // If no config file exists, we're done
+            if (!File.Exists(ConfFile)) return;
+
+            // Try loading the values from file
+            try
+            {
+                using (JsonDocument document = JsonDocument.Parse(File.ReadAllText(ConfFile)))
+                {
+                    JsonElement value;
+                    if (document.RootElement.TryGetProperty(nameof(_model.ClientInstallPath), out value))
+                    {
+                        _model.ClientInstallPath = value.GetString();
+                    }
+                    if (document.RootElement.TryGetProperty(nameof(_model.ClientDBPath), out value))
+                    {
+                        _model.ClientDBPath = value.GetString();
+                    }
+                    if (document.RootElement.TryGetProperty(nameof(_model.LogPath), out value))
+                    {
+                        _model.LogPath = value.GetString();
+                    }
+                }
             }
-            else
+            catch (Exception ex)
             {
-                try
-                {
-                    _model = JsonSerializer.Deserialize<PathConfModel>(File.ReadAllText(ConfFile));
-                }
-                catch (Exception ex)
-                {
-                    Log.Error($"Error deserializing path config file:\r\n{ex}");
-                }
-                finally
-                {
-                    if (_model == null)
-                        _model = new PathConfModel();
+                Log.Error($"Error deserializing path config file:\r\n{ex}");
+            }
+            finally
+            {
+                if (_model == null)
+                    _model = new PathConfModel();
 
-                    if (string.IsNullOrEmpty(_model.ClientInstallPath))
-                        _model.ClientInstallPath = DefaultClientInstallPath;
+                // Sanitize loaded values  
+                if (string.IsNullOrEmpty(_model.ClientInstallPath)) _model.ClientInstallPath = DefaultClientInstallPath;
+                if (string.IsNullOrEmpty(_model.ClientDBPath)) _model.ClientDBPath = DefaultClientDBPath;
+                if (string.IsNullOrEmpty(_model.LogPath)) _model.LogPath = DefaultLogPath;
 
-                    if (string.IsNullOrEmpty(_model.ClientDBPath))
-                        _model.ClientDBPath = DefaultClientDBPath;
-                }
+                // Since loading may have sanitized or amended new values, we
+                // save the config here to that changes are written back to file.
+                SaveConfig();
             }
         }
 
@@ -189,7 +229,8 @@ namespace SQRLCommon.Models
             string serialized = JsonSerializer.Serialize(_model, _model.GetType(), options);
             File.WriteAllText(ConfFile, serialized);
 
-            // Because in Linux Root Owns Everything we need to change the owner of the config back to our current user
+            // Because in Linux "Root Owns Everything", we need to change the owner of the 
+            // config back to our current user
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && SystemAndShellUtils.IsAdmin())
             {
                 var user = SystemAndShellUtils.GetCurrentUser();
@@ -214,5 +255,10 @@ namespace SQRLCommon.Models
         /// The directory where the SQRL client's database is located.
         /// </summary>
         public string ClientDBPath { get; set; } = PathConf.DefaultClientDBPath;
+
+        /// <summary>
+        /// The directory where the SQRL client and installer store their log files.
+        /// </summary>
+        public string LogPath { get; set; } = PathConf.DefaultLogPath;
     }
 }
